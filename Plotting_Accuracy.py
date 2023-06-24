097a5e1ce9e73fb1d1c679241f7952c0168fbbd0
Arpita kesharwani <107834813+KesharwaniArpita@users.noreply.github.com>
2023-06-24 02:50:15 -0700

    Update and rename Plotting.py to Plotting_Accuracy.py

--- Plotting_Accuracy.py
+++ Plotting_Accuracy.py
@@ -2,12 +2,11 @@ def plot_accuracy(history, model_name):
     train_acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
     epochs = range(1, len(train_acc) + 1)
-    test_acc = history.history['val_accuracy'][-1] * np.ones(len(train_acc))
+    
 
     # Plot the accuracy curves
     plt.plot(epochs, train_acc, 'b-', label='Training Accuracy')
-    plt.plot(epochs, val_acc, 'r-', label='Validation Accuracy')
-    plt.plot(epochs, test_acc, 'g-', label='Test Accuracy')
+    plt.plot(epochs, val_acc, 'g-', label='Validation Accuracy')
     plt.title(f'Model Accuracy - {model_name}')
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
